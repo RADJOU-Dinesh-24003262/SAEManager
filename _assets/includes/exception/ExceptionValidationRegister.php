eeedac93d2fcba40fc6b49ade548a1f81aa1d1f8
Dinesh <dinesh.radjou@etu.univ-amu.fr>
2025-09-23 22:16:15 +0200

    Refactor registration validation and improve autoloading mechanism + better Exception handle

--- _assets/includes/exception/ExceptionValidationRegister.php
+++ _assets/includes/exception/ExceptionValidationRegister.php
@@ -1,11 +1,11 @@
 <?php
-namespace _assets\includes\exeption;
+namespace includes\exception;
 
-class ExeptionValidationRegister extends \Exception{
+class ExceptionValidationRegister extends \Exception{
     public function __construct(
         private string $field,
         private string $type,
         private string $additionalInfo = '') {
-        parent::__construct($message);
+        parent::__construct($additionalInfo);
     }
 }
