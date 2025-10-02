e7f202fffceb4ca1e9992ff7bd2dd8b9a00ad18e
Dinesh <dinesh.radjou@etu.univ-amu.fr>
2025-10-02 15:51:23 +0200

    Fix validation logic for 'td' field in ValidationServiceRegister

--- modules/src/Utilis/ValidationServiceRegister.php
+++ modules/src/Utilis/ValidationServiceRegister.php
@@ -98,7 +98,7 @@ class ValidationServiceRegister
                 $errors[] = new ExceptionValidationRegister('parcours', 'string', "Parcours requis en BUT 2 et BUT 3.");
             } elseif (!$this->isValidParcours($data['parcours'])) {
                 $errors[] = new ExceptionValidationRegister('parcours', 'string', "Parcours invalide.");
-            } elseif ($data['td'] ?? '' === 'TD4') {
+            } elseif (($data['td'] ?? '') === 'TD4') {
                 $errors[] = new ExceptionValidationRegister('td', 'string', "TD4 uniquement disponible en BUT 1.");
             }
         } elseif (!empty($data['parcours'])) {
