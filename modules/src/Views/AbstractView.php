363b9e2068e6a9df438e506679b5f4e67210b8d0
dinesh <dinesh.radjou@etu.univ-amu.fr>
2025-10-02 08:50:17 +0200

    better assets architecture

--- modules/src/Views/AbstractView.php
+++ modules/src/Views/AbstractView.php
@@ -45,7 +45,7 @@ abstract class AbstractView
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>' . $this->getPageTitle() . '</title>
-            <link rel="stylesheet" href="_assets/styles/'. $this->getNameCss().'">
+            <link rel="stylesheet" href="styles/'. $this->getNameCss().'">
             ' . $this->getAdditionalHeaders() . '
         </head>
         <body>';
