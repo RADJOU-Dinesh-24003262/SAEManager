4c980f9023d8341ec41abee74e5c38e05b20fc0a
e24011695 <williapro0@gmail.com>
2025-10-06 21:29:06 +0200

    Add favicon to site and update AbstractView
    Added favicon.ico to the image directory and updated AbstractView.php to include a link to the favicon in the HTML head. This improves site branding and browser tab visibility.

--- modules/src/Views/AbstractView.php
+++ modules/src/Views/AbstractView.php
@@ -45,6 +45,7 @@ abstract class AbstractView
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>' . $this->getPageTitle() . '</title>
+            <link rel="icon" type="image/x-icon" href="/image/favicon.ico">
             <link rel="stylesheet" href="styles/'.$this->getNameCss().'">
             <link rel="stylesheet" href="styles/header.css">
             ' . $this->getAdditionalHeaders() . '
