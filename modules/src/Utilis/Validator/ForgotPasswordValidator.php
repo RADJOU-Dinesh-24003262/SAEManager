b1f9e1be79974fcd5f2ecb30df2667832d54c5cf
BENHAFESSA Alexandre <ripptact@gmail.com>
2025-10-16 18:50:52 +0200

    Added documentation Utilis

--- modules/src/Utilis/Validator/ForgotPasswordValidator.php
+++ modules/src/Utilis/Validator/ForgotPasswordValidator.php
@@ -3,10 +3,36 @@ namespace Utilis\Validator;
 
 use includes\exception\ExceptionValidationForgotPassword;
 
+/**
+ * Class ForgotPasswordValidator
+ *  
+ * @package     src
+
+ * @subpackage  Utilis\Validator
+
+ * @author      Benhafessa Alexandre, Dargentolle Francois, Edelstein William, Griguer Nathan, Radjou Dinesh
+ * 
+ * Provides method to validate the inputed form infos on the forgot password page.
+ */
 class ForgotPasswordValidator extends FormValidator
 {
+    /**
+     * The list of the variables required for the password reset of a user.
+     * @var array
+     */
     protected $required = ['email'];
 
+    /**
+     * 
+     * 
+     * This this method validated the values given in $data to let a user try to reset their password with.
+     *
+     * @param array $data array, in adequation to the required value fields. 
+     * 
+     * @return void
+     * 
+     * @throws ExceptionValidationForgotPassword all the errors that might have been found
+     */
     public function validate(array $data): void
     {
         $errors = [];
