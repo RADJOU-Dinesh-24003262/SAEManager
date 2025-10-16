d1bc0e6affac76f8ca3101f3ecd7937d3add9716
dinesh <dinesh.radjou@etu.univ-amu.fr>
2025-10-16 11:45:22 +0200

    Fix : Add documentation

--- _assets/includes/exception/ExceptionEmailSendingFailed.php
+++ _assets/includes/exception/ExceptionEmailSendingFailed.php
@@ -2,8 +2,29 @@
 
 namespace includes\exception;
 
-class ExceptionEmailSendingFailed extends \Exception {
-    public function __construct(string $message = "Erreur lors de l'envoi de l'email. Veuillez réessayer plus tard.") {
+/**
+ * Class ExceptionEmailSendingFailed
+ *
+ * Custom exception thrown when an email fails to send.
+ *
+ * This exception should be used to indicate an error occurred during
+ * the process of sending an email. It can help differentiate between
+ * general exceptions and issues specifically related to email functionality.
+ *
+ * @package includes\exception
+ */
+class ExceptionEmailSendingFailed extends \Exception
+{
+    /**
+     * ExceptionEmailSendingFailed constructor.
+     *
+     * Initializes the exception with a default or custom error message.
+     *
+     * @param string $message Optional. A custom error message.
+     * Defaults to: "Erreur lors de l'envoi de l'email. Veuillez réessayer plus tard."
+     */
+    public function __construct(string $message = "Erreur lors de l'envoi de l'email. Veuillez réessayer plus tard.")
+    {
         parent::__construct($message);
     }
 }
