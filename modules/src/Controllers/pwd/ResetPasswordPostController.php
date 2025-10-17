7ec781ede56fd0e8428c109b4b81ca22fae613a4
BENHAFESSA Alexandre <ripptact@gmail.com>
2025-10-17 10:24:37 +0200

    Added documentation (ALL)

--- modules/src/Controllers/pwd/ResetPasswordPostController.php
+++ modules/src/Controllers/pwd/ResetPasswordPostController.php
@@ -14,8 +14,24 @@ use includes\exception\ExceptionValidationEmptys;
 use includes\exception\ExceptionInvalidToken;
 use includes\exception\ExceptionPasswordUpdateFailed;
 
+/**
+ * Class User
+ 
+ * @package     src
+
+ * @subpackage  Controllers\pwd
+
+ * @author      Benhafessa Alexandre, Dargentolle Francois, Edelstein William, Griguer Nathan, Radjou Dinesh
+
+ * This class controls the reset password process (post).
+ */
 class ResetPasswordPostController implements ControllerInterface
 {
+    /**
+     * Principal manager of the controller
+     * 
+     * @return void
+     */
     public function control(): void
     {
         try {
@@ -81,6 +97,11 @@ class ResetPasswordPostController implements ControllerInterface
         (new ResetPasswordView($token, $email))->render();
     }
 
+    /**
+     * Check if this controller can handle the request
+     * 
+     * @return boolean Is the method post?
+     */
     public static function support(string $chemin, string $method): bool
     {
         return $chemin === "/reset-password" && $method === "POST";
--- modules/src/Models/User/UserRepository.php
+++ modules/src/Models/User/UserRepository.php
@@ -3,15 +3,48 @@ namespace Models\User;
 
 use includes\database;
 
+/**
+ * Class UserRepository
+ 
+ * @package     src
+
+ * @subpackage  Models\User
+
+ * @author      Benhafessa Alexandre, Dargentolle Francois, Edelstein William, Griguer Nathan, Radjou Dinesh
+
+ * This class regroup function to manage the users in the database
+ */
 class UserRepository
 {
+    /**
+     * Connection to the database, storred in this database object
+     * @var database
+     */
     private database $db;
 
+    /**
+     * Creates an instance of the class
+     * 
+     * This method constructs a user repository object, affecting the database given in parametters
+     * to the db variable.
+     * 
+     * @param database $db The database to instanciate
+     */
     public function __construct(database $db)
     {
         $this->db = $db;
     }
     
+    /**
+     * Returns the existance of the given email
+     * 
+     * Looks for the email given in parametters in the database, return true if the email exists. False if it is not found
+     * or if an error occurs.
+     * 
+     * @param database $db The database to instanciate
+     * 
+     * @return boolean
+     */
     public function existsByEmail(string $email): bool
     {
         try {
@@ -24,6 +57,16 @@ class UserRepository
         }
     }
     
+    /**
+     * Returns the success of th insertion of a user in the database
+     * 
+     * Tries to insert a new user in the database with the user given in parametters.
+     * Return true if it succeed, false otherwise.
+     * 
+     * @param User $user The user object to insert
+     * 
+     * @return boolean
+     */
     public function save(User $user): bool
     {
         try {
