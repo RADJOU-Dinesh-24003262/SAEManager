6116e4b0c0b23066f3f5f9e17affbdcb20b188fa
François Dargentolle <francois.dargentolle@etu.univ-amu.fr>
2025-10-21 17:29:15 +0200

    Fix : Fix : PhpDoc for the controllers of PageSae and SujetSae - part 4

--- modules/src/Controllers/SaeSujet/SaeSujetController.php
+++ modules/src/Controllers/SaeSujet/SaeSujetController.php
@@ -14,7 +14,6 @@ use Views\ToDoList\ToDoListView;
  *              William Edelstein <william.edelstein@etu.univ-amu.fr>,
  *              Nathan Griguer <nathan.griguer@etu.univ-amu.fr>,
  *              Dinesh Radjou <dinesh.radjou@etu.univ-amu.fr>
- * @see SaeSujetView
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License v3.0
  * @link /sae-sujet
  * Controller for the form of the subject of the SAE.
@@ -35,8 +34,8 @@ class SaeSujetController implements ControllerInterface
     /**
      *  @method static bool support(string $chemin, string $method)
      *  Determines if this controller supports the given path and method.
-     *  @param string $path   The requested path.
-     *  @param string $method   The HTTP method.
+     *  @param string $path The requested path.
+     *  @param string $method The HTTP method.
      *  @return boolean true if the path and method are supported, false otherwise.
      */
     public static function support(string $path, string $method): bool
