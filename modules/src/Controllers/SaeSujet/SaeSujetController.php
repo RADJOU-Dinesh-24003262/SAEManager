0ab4ca428d162a3440c97958b3b3636db568c91e
Nathangr06 <nathan.griguer@etu.univ-amu.fr>
2025-10-21 20:13:33 +0200

    Fix : fix check with phpdoc

--- modules/src/Controllers/SaeSujet/SaeSujetController.php
+++ modules/src/Controllers/SaeSujet/SaeSujetController.php
@@ -34,8 +34,9 @@ class SaeSujetController implements ControllerInterface
     /**
      *  @method static bool support(string $chemin, string $method)
      *  Determines if this controller supports the given path and method.
-     *  @param string $path The requested path.
+     *  @param string $path   The requested path.
      *  @param string $method The HTTP method.
+     *
      *  @return boolean true if the path and method are supported, false otherwise.
      */
     public static function support(string $path, string $method): bool
