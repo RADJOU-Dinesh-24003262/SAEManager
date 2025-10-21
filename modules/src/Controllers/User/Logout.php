55c04a7655e25433c87b007f1b9c516a1d8841db
dinesh <dinesh.radjou@etu.univ-amu.fr>
2025-10-21 10:33:59 +0200

    Fix : fix for phpcs and phpstan

--- modules/src/Controllers/User/Logout.php
+++ modules/src/Controllers/User/Logout.php
@@ -7,13 +7,15 @@ use Utilis\SessionService;
 use Views\Index\IndexView;
 
 /**
- * Logout controller
+ * Logout controller.
  *
  * Handles user logout (GET request).
  *
+ * @category Controller
  * @package Controllers\User
- * @author Benhafessa Alexandre, Dargentolle Francois,
- *         Edelstein William, Griguer Nathan, Radjou Dinesh
+ * @author Radjou Dinesh <dinesh.radjou@etu.univ-amu.fr>
+ * @license MIT https://opensource.org/licenses/MIT
+ * @link https://github.com/RADJOU-Dinesh-24003262/SAEManager/
  */
 class Logout implements ControllerInterface
 {
@@ -24,30 +26,30 @@ class Logout implements ControllerInterface
      */
     public function control(): void
     {
-        // Redirect to login page if user is not logged in
+        // Redirect to login page if user is not logged in.
         if (!SessionService::has('user_id')) {
             SessionService::setFlash('errors', ['Veuillez vous connecter d\'abord']);
             header('Location: /login');
             exit();
         }
 
-        SessionService::setFlash('success', 'Vous avez été bien déconnecter de votre session.');
+        SessionService::setFlash('success', 'Vous avez été bien déconnecté de votre session');
 
-        // Render logout view
+        // Render logout view.
         $view = new IndexView();
         $view->render();
 
-        // Clear session
-        session_unset();     // Unset all session variables
-        session_destroy();   // Destroy the session
+        // Clear session.
+        session_unset();     // Unset all session variables.
+        session_destroy();   // Destroy the session.
     }
 
     /**
      * Check if this controller should handle the current request
      *
-     * @param string $path   The request path
-     * @param string $method The HTTP request method
-     * @return bool True if path is /logout and method is GET
+     * @param string $path   The request path.
+     * @param string $method The HTTP request method.
+     * @return boolean True if path is /logout and method is GET.
      */
     public static function support(string $path, string $method): bool
     {
