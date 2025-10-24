f67c048ee9742d53d0b6604dc8521ff24ebde4a0
François <francois.dargentolle@etu.univ-amu.fr>
2025-10-24 15:05:11 +0200

    Feat : WIP : Add the fixed and last version of the ToDoListPost (needs now the tests)

--- modules/src/Controllers/ToDoList/ToDoListPost.php
+++ modules/src/Controllers/ToDoList/ToDoListPost.php
@@ -11,32 +11,58 @@ use Utilis\Validator\ToDoListValidator;
 use Views\ToDoList\ToDoListView;
 use includes\exception\ExceptionValidation;
 
+/**
+ * This class controls the todolist process (post).
+
+ * @category Controller
+
+ * @package Src
+
+ * @subpackage Controllers\ToDoList
+
+ * @author  Alexandre Benhafessa <alexandre.benhafessa@etu.univ-amu.fr>,
+ * @author  François Dargentolle <francois.dargentolle@etu.univ-amu.fr>,
+ * @author  William Edelstein <william.edelstein@etu.univ-amu.fr>,
+ * @author  Nathan Griguer <nathan.griguer@etu.univ-amu.fr>,
+ * @author  Dinesh Radjou <dinesh.radjou@etu.univ-amu.fr>
+
+ * @license MIT License https://opensource.org/licenses/MIT
+
+ * @link https://github.com/RADJOU-Dinesh-24003262/SAEManager
+ */
 class ToDoListPost implements ControllerInterface
 {
-
-
+    /**
+     * Principal manager of the controller
+     *
+     * @return void
+     *
+     * @throws \Exception For any other unexpected errors during the save of the task process.
+     */
     public function control(): void
     {
+        // Validate the data
         $validator = new ToDoListValidator();
 
-        try{
+        try {
             $data = $validator->escape($_POST);
             $validator->validate($data);
 
+            // Create the ToDoList
             $todolist = ToDoList::create($data);
 
+            // Save the ToDoList
             if ($todolist->save()) {
-                error_log("Nouvel tâche enregistré : " . $todolist.getToDoId());
+                error_log("Nouvel tâche enregistré : " . $todolist->getToDoId());
                 $view = new ToDoListView($todolist);
                 $view->render();
 
                 return;
             } else {
-                error_log("Erreur tâche enregistré : " . $todolist.getToDoId());
+                error_log("Erreur tâche enregistré : " . $todolist->getToDoId());
                 throw new \Exception("Erreur lors de la sauvegarde");
             }
-        }
-        catch (ExceptionValidation | ExceptionValidationEmpty $e){
+        } catch (ExceptionValidation | ExceptionValidationEmpty $e) {
             $errors = [];
             foreach ($e->getErrors() as $error) {
                 $errors[] = $error->getMessage();
@@ -51,6 +77,14 @@ class ToDoListPost implements ControllerInterface
         $view->render();
     }
 
+    /**
+     * Determines whether this controller supports the given request.
+     *
+     * @param string $path   The requested URI path.
+     * @param string $method The HTTP method used in the request.
+     *
+     * @return boolean True if the path is "/to-do-list" and the method is POST.
+     */
     public static function support(string $path, string $method): bool
     {
         return $path === "/to-do-list" && $method === "POST";
