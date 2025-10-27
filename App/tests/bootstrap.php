c74e9f120589112bcfca0ab4b3111cb0df543a26
François Dargentolle <francois.dargentolle@etu.univ-amu.fr>
2025-10-27 13:54:02 +0100

    Fix : problems with the bootstrap - part 2

--- App/tests/bootstrap.php
+++ App/tests/bootstrap.php
@@ -15,7 +15,7 @@ define('APP_ENV', 'testing');
 
 // Load Composer autoloader
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../_assets/includes/Autoloader.php';
+require_once __DIR__ . '/../../_assets/includes/Autoloader.php';
 
 // Start session for tests
 if (session_status() === PHP_SESSION_NONE) {
