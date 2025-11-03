8cdbf9f149c2153ed1e527bc8682afd7ed8d3eea
Nathangr06 <nathan.griguer@etu.univ-amu.fr>
2025-11-03 14:48:59 +0100

    Fix: test github action

--- App/src/Models/User/User.php
+++ App/src/Models/User/User.php
@@ -315,23 +315,6 @@ abstract class User
         }
     }
 
-    public static function deleteByEmail(string $email): void
-    {
-        try {
-            $db = Database::getInstance();
-            $stmt = $db->prepare('DELETE FROM users WHERE email = :email; ');
-
-            $stmt->execute(['email' => $email]);
-
-            if ($stmt->rowCount() === 0) {
-                throw new Exception('Aucun utilisateur trouvé avec cet email.');
-            }
-        } catch (PDOException $e) {
-            error_log('Erreur suppression du compte utilisateur : ' . $e->getMessage());
-            throw new Exception('Erreur lors de la suppression du compte utilisateur.');
-        }
-    }
-
     // -----------------
     // Getters
     // -----------------
