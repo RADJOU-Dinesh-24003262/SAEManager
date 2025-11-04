84ae76a6537a1b04c2030a2471d468d167ad1d95
BENHAFESSA-Alexandre <alexandre.benhafessa@etu.univ-amu.fr>
2025-11-04 12:02:36 +0100

    Add: new functionality for the SAEs. WIP

---
+++ Core/includes/exception/ExceptionValidation/ExeptionValidationSAECreation.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Core\includes\exception\ExceptionValidation;
+
+/**
+ * ExceptionValidationEmpty
+ *
+ * Custom exception thrown when the SAE creation process fails.
+ *
+ * @category   Exception
+ * @package    Core
+ * @subpackage Includes\Exception\ExceptionValidation
+ * @author     Alexandre Benhafessa <alexandre.benhafessa@etu.univ-amu.fr>
+ * @author     François Dargentolle <francois.dargentolle@etu.univ-amu.fr>
+ * @author     William Edelstein <william.edelstein@etu.univ-amu.fr>
+ * @author     Nathan Griguer <nathan.griguer@etu.univ-amu.fr>
+ * @author     Dinesh Radjou <dinesh.radjou@etu.univ-amu.fr>
+ * @license    MIT License https://opensource.org/licenses/MIT
+ * @link       https://github.com/RADJOU-Dinesh-24003262/SAEManager
+ */
+class ExeptionValidationSAECreation extends \Exception
+{
+
+    /**
+     * @var string Additional information about the SAE creation validation failure
+     */
+    private string $additionalInfo;
+
+    /**
+     * ExeptionValidationSAECreation constructor.
+     *
+     * @param  string $additionalInfo Optional. Additional details about the error.
+     *                                Defaults to "Invalid fields".
+     * @return void
+     */
+    public function __construct(string $additionalInfo = 'Invalid fields.')
+    {
+        $this->additionalInfo = $additionalInfo;
+        parent::__construct($additionalInfo);
+    }
+
+    /**
+     * Returns additional information about the validation failure.
+     *
+     * @return string The additional information provided at exception creation
+     */
+    public function getAdditionalInfo(): string
+    {
+        return $this->additionalInfo;
+    }
+}
