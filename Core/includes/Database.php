abf09f0a5845fa9c977cd99b2745e220bcaa176b
saemanager <saemanager@ssh1.paris1.alwaysdata.com>
2026-03-04 10:11:25 +0100

    Fix : repair the ini file acces in the database.php

--- Core/includes/Database.php
+++ Core/includes/Database.php
@@ -47,7 +47,7 @@ class Database extends PDO
      * @param string $file Path to the configuration INI file.
      * @throws Exception If the configuration file cannot be read or connection fails.
      */
-    public function __construct(string $file = '../App/config/my_settings.ini')
+    public function __construct(string $file = '../../App/config/my_settings.ini')
     {
         // --- Test mode ---
         if (getenv('APP_ENV') === 'testing') {
@@ -63,9 +63,10 @@ class Database extends PDO
             return;
         }
 
-        // --- Normal mode ---
-        $settings = parse_ini_file($file, true);
-        if ($settings === false) {
+
+	// --- Normal mode ---
+        $settings = parse_ini_file(__DIR__ . '/' . $file, true);
+	if ($settings === false) {
             throw new Exception('Unable to open configuration file: ' . $file);
         }
 
@@ -99,7 +100,7 @@ class Database extends PDO
      * @return Database The singleton instance.
      * @throws Exception If connection fails.
      */
-    public static function getInstance(string $file = '../App/config/my_settings.ini'): Database
+    public static function getInstance(string $file = '../../App/config/my_settings.ini'): Database
     {
         if (self::$instance === null) {
             self::$instance = new self($file);
