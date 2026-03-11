c1ace83ec9131ba05faac3d9767b234f843803cf
Griguer Nathan <nathan.griguer@etu.univ-amu.fr>
2026-03-11 11:09:53 +0100

    Fix: remove useless verifications

--- App/src/Models/Entity/User/UserFactory.php
+++ App/src/Models/Entity/User/UserFactory.php
@@ -39,14 +39,6 @@ class UserFactory
     public static function create(array $data): User
     {
 
-        // Map pending_registrations fields to User entity fields.
-        if (isset($data['status']) && !isset($data['user_type'])) {
-            $data['user_type'] = $data['status'];
-        }
-        if (isset($data['password']) && !isset($data['hashed_password'])) {
-            $data['hashed_password'] = $data['password'];
-        }
-
         $type = $data['user_type'] ?? 'student';
 
         $className = self::$typeRegistry[$type];
